219b27b814942c356abfac4897cba20ad6f44884
roessle1 <basti.roessler@t-online.de>
2023-04-11 15:27:31 +0200

    trying stream

---
+++ public/assets/song_control.js
@@ -0,0 +1,29 @@
+let song = new Howl({
+    src: []
+});
+let isPlaying = false;
+
+function play(url) {
+    /*const response = await axios.get("/music/Music" + url, {
+        responseType: 'arraybuffer',
+    });
+    // create audioBuffer (decode audio file)
+    const audioBuffer = await audioContext.decodeAudioData(response.data);*/
+
+    song = new Howl({
+        src: [url],
+        html5: false,
+    });
+    song.play();
+}
+
+function play_pause(url) {
+    if(isPlaying) {
+        isPlaying = false;
+        song.stop();
+    } else {
+        isPlaying = true;
+        play(url);
+    }
+}
+
