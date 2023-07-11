1c6f99e0c079547f14aae49f6ee147515f0cf64f
roessle1 <basti.roessler@t-online.de>
2023-07-11 14:32:12 +0200

    implemented track skipping, play/pause, the repeating mode and random playing mode

--- src/Controller/Admin/TrackCrudController.php
+++ src/Controller/Admin/TrackCrudController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Track;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use function Sodium\add;
 
 class TrackCrudController extends AbstractCrudController
 {
@@ -22,5 +25,18 @@ class TrackCrudController extends AbstractCrudController
         yield IntegerField::new('track_number');
         yield IntegerField::new('duration');
         yield TextField::new('path');
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPaginatorPageSize(50);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('album')
+            ->add('track_number');
     }
 }
